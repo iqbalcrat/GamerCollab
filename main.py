1be7441903503dc168afa378d13fc8b77ca30b94
iqbal honnur <iqbalhonnur@gmail.com>
2020-05-16 23:20:58 +0530

    prod commit

--- main.py
+++ main.py
@@ -1,20 +1,47 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
+import pyrebase
 
+
+
+config = {
+    'apiKey': 'AIzaSyCrggDa5PaxM2mreGr8ESJVQha1OWoS4Uc',
+    'authDomain': 'gamercollab-e8a25.firebaseapp.com',
+    'databaseURL': 'https://gamercollab-e8a25.firebaseio.com',
+    'projectId': 'gamercollab-e8a25',
+    'storageBucket': 'gamercollab-e8a25.appspot.com',
+    'messagingSenderId': '687236169723',
+    'appId': '1:687236169723:web:1dfab4d6055deb57b5555e',
+    'measurementId': 'G-YH9314G9ZF'
+}
 
 app = Flask(__name__)
+firebase = pyrebase.initialize_app(config)
+db = firebase.database()
 
 
-@app.route("/")
-@app.route("/home")
+@app.route("/", methods = ["GET", "POST"])
+@app.route("/home", methods = ["GET", "POST"])
 def home():
     return render_template("index.html")
-
 
 @app.errorhandler(404)
 def page_not_found(e):
     # note that we set the 404 status explicitly
     return render_template('404.html'), 404
 
+@app.route("/register", methods = ["GET", "POST"])
+def register():
+    if request.method == "POST":
+        email=request.form["email"]
+        if email:
+            db.child("email").push(email)
+            success = "Thanks for you interest in GamerCollab, we will notify you once the beta product is ready."
+            return render_template('index.html', success=success)
+        else:
+            error = "Kinldy enter your email id."
+            return render_template("index.html", error=error)
+    else:
+        return render_template("404.html")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
