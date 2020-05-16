4622aa42b2785dd43d34a195e25b8189a8afcef8
iqbal honnur <iqbalhonnur@gmail.com>
2020-05-16 20:44:43 +0530

    first python commit

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template("index.html")
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return render_template('404.html'), 404
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
